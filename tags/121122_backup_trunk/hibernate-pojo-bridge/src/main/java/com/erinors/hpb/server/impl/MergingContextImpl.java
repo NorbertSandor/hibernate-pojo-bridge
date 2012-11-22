d43dbb7c927b45b2b1e73a50f33d7530f612bcff
sandor.norbert@erinors.com <sandor.norbert@erinors.com@78911f08-42f7-11de-b466-9154fc769079>
2012-11-22 13:11:17 +0000

    backup trunk

---
+++ tags/121122_backup_trunk/hibernate-pojo-bridge/src/main/java/com/erinors/hpb/server/impl/MergingContextImpl.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2009 Norbert Sándor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.erinors.hpb.server.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.engine.spi.SessionImplementor;
+
+/**
+ * @author Norbert Sándor
+ */
+public class MergingContextImpl extends AbstractContext implements MergingContext
+{
+    private final EntityManager entityManager;
+
+    protected MergingContextImpl(List<? extends PersistentObjectHandler> handlers, EntityManager entityManager)
+    {
+        super(handlers);
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Object merge(Object object)
+    {
+        if (object == null)
+        {
+            return null;
+        }
+
+        if (isProcessed(object))
+        {
+            return getProcessedObject(object);
+        }
+        else
+        {
+            Object merged = null;
+            for (PersistentObjectHandler handler : getHandlers())
+            {
+                merged = handler.merge(this, object);
+
+                if (merged == PersistentObjectHandler.ProcessedToNull)
+                {
+                    return null;
+                }
+                else if (merged != null)
+                {
+                    return merged;
+                }
+            }
+
+            throw new RuntimeException("Cannot merge object of unsupported type: " + object);
+        }
+    }
+
+    @Override
+    public EntityManager getEntityManager()
+    {
+        return entityManager;
+    }
+
+    @Override
+    public SessionImplementor getSessionImplementor()
+    {
+        assert getEntityManager().getDelegate() instanceof SessionImplementor : "Unsupported EntityManager, an instance of SessionImplementor is expected but got "
+                + getEntityManager();
+        return (SessionImplementor) getEntityManager().getDelegate();
+    }
+}
---
+++ tags/121122_backup_trunk/hibernate-pojo-bridge/src/test/java/com/erinors/hpb/tests/integration/case0002/LazyManyToManyListTest3.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2009 Norbert Sándor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.erinors.hpb.tests.integration.case0002;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.collection.internal.PersistentList;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.erinors.hpb.server.api.PersistentObjectManager;
+import com.erinors.hpb.test.EmptyPersistentObjectManager;
+import com.erinors.hpb.test.HpbTestUtils;
+import com.erinors.hpb.test.SqlAppender;
+import com.erinors.hpb.tests.integration.HpbIntegrationTestCase;
+
+public class LazyManyToManyListTest3 extends HpbIntegrationTestCase
+{
+    private static List<List<String>> sqls = new ArrayList<List<String>>();
+
+    @Test
+    public void testAddAndRemove()
+    {
+        testAddAndRemove(new EmptyPersistentObjectManager());
+        List<String> sql = SqlAppender.get().getSql();
+        sqls.add(sql);
+    }
+
+    @Test
+    public void testAddAndRemove2()
+    {
+        testAddAndRemove(getPersistentObjectManager());
+        List<String> sql = SqlAppender.get().getSql();
+        sqls.add(sql);
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        HpbTestUtils.assertHaveSameDml(sqls.get(0), sqls.get(1));
+    }
+
+    private void testAddAndRemove(final PersistentObjectManager persistentObjectManager)
+    {
+        //
+        // Populate database
+        //
+
+        getPersistenceService().executeTransactionally(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Parent parent = (Parent) new Parent().presetId(100);
+                parent.getChildren().add((Child) new Child(0).presetId(101));
+                parent.getChildren().add((Child) new Child(1).presetId(102));
+                parent.getChildren().add((Child) new Child(2).presetId(103));
+                getEntityManager().persist(parent);
+            }
+        });
+
+        Assert.assertEquals(Arrays.asList("insert into Parent (version, id) values (0, 100)",
+                "insert into Child (version, value, id) values (0, 0, 101)",
+                "insert into Child (version, value, id) values (0, 1, 102)",
+                "insert into Child (version, value, id) values (0, 2, 103)",
+                "insert into Parent_Child (Parent_id, childIndex, children_id) values (100, 0, 101)",
+                "insert into Parent_Child (Parent_id, childIndex, children_id) values (100, 1, 102)",
+                "insert into Parent_Child (Parent_id, childIndex, children_id) values (100, 2, 103)"),
+                SqlAppender.get().getSql());
+
+        //
+        // Clone
+        //
+
+        final Parent[] detached = new Parent[1];
+
+        getPersistenceService().executeTransactionally(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Parent parent = getEntityManager().find(Parent.class, 100L);
+                Assert.assertTrue(parent.getChildren() instanceof PersistentList);
+                Assert.assertFalse(((PersistentList) parent.getChildren()).wasInitialized());
+
+                detached[0] = persistentObjectManager.clone(parent);
+            }
+        });
+
+        SqlAppender.get().clearSql();
+
+        //
+        // Merge
+        //
+
+        getPersistenceService().executeTransactionally(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                getEntityManager().merge(persistentObjectManager.merge(detached[0]));
+            }
+        });
+
+        List<String> sql = SqlAppender.get().getSql();
+        Assert.assertEquals(1, sql.size());
+        Assert.assertTrue(sql.get(0).startsWith("select"));
+    }
+}
